76f7e37939723c785e0bb1b2ea651f15812cbc53
hp <nikhil.raj@chqbook.com>
2023-01-18 21:24:41 +0530

    changed normal gridview to staggered gridview

--- lib/main.dart
+++ lib/main.dart
@@ -23,3 +23,17 @@ class MyApp extends StatelessWidget {
   }
 }
 
+
+// StaggeredGridView.countBuilder(
+// crossAxisCount: 2,
+// crossAxisSpacing: 10,
+// mainAxisSpacing: 12,
+// itemCount: 50,
+// itemBuilder: (context, index) {
+// return index.isEven?Text("helloworStaggeredTile.fit(2)StaggeredTile.fit(2)StaggeredTile.fit(2)StaggeredTile.fit(2)ld",style: TextStyle(color: Colors.deepOrange,backgroundColor: Colors.blue),):Text("This video shows how to create a staggered gridview in a flutter and shows how to staggered gridview will work when using the flutter_staggered_grid_view package, and then they will be shown on your device.");
+// }, //staggeredTileBuilder: (int index) { StaggeredTile.fit(2); },
+// staggeredTileBuilder: (index) {
+// return StaggeredTile.fit(1);
+// }
+// ),
+
--- lib/pages/notes.dart
+++ lib/pages/notes.dart
@@ -5,6 +5,7 @@ import 'package:notes_demo/models/noteModel.dart';
 import 'package:notes_demo/utils/database_helper.dart';
 import 'package:sliding_sheet/sliding_sheet.dart';
 import 'package:sqflite/sqflite.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class NotesList extends StatefulWidget {
   const NotesList({Key? key}) : super(key: key);
@@ -20,7 +21,6 @@ class _NotesListState extends State<NotesList> {
 
   @override
   Widget build(BuildContext context) {
-
     if (noteList == null) {
       noteList = <Note>[];
       updateListView();
@@ -32,72 +32,156 @@ class _NotesListState extends State<NotesList> {
           title: const Text("UserNotes"),
           centerTitle: true,
         ),
+        body: StaggeredGridView.countBuilder(
+            crossAxisCount: 2,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 12,
+            itemCount: noteList!.length,
+            itemBuilder: (context, index) {
+              return GestureDetector(
+                child: Container(
+                  margin: const EdgeInsets.all(5),
+                  child: Card(
+                    elevation: 20,
 
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const SizedBox(height: 5,),
-            const Text("Single Tap to Update"),
-            const Text("Double Tap to Delete"),
-            const SizedBox(height: 5,),
-            Expanded(
-              child: GridView.builder(
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-                  itemCount: noteList!.length,
-                  itemBuilder: (context, index) {
-                    return GestureDetector(
-                      child: Container(
-                        margin: const EdgeInsets.all(10),
-                        width: 160,
-                        height: 160,
-                        child: Card(
-                          elevation: 20,
-                          color: Colors.white70, //Color(0xff252525),
+                    color: Colors.white70, //Color(0xff252525),
 
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20),
-
-                          ),
-                          child: Container(
-                            //alignment: Alignment.center,
-                            margin: EdgeInsets.all(5),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: <Widget>[
-                                Text(noteList![index].title,textAlign: TextAlign.center,style: const TextStyle(fontWeight: FontWeight.w900,fontSize: 20,color: Colors.black),maxLines: 1,overflow: TextOverflow.ellipsis,),
-                                const SizedBox(height: 5,),
-                                Text(noteList![index].date,style: const TextStyle(fontWeight: FontWeight.w600,fontSize: 10,color: Colors.black54),),
-                                const SizedBox(height: 20,),
-                                Container(padding: EdgeInsets.all(5),child: Text(noteList![index].description,textAlign: TextAlign.center,style: const TextStyle(fontWeight: FontWeight.w700,fontSize: 14,color: Colors.black87),maxLines: 3,overflow: TextOverflow.ellipsis,)),
-                              ],
-                            ),
-                          ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Container(
+                        child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: <Widget>[
+                        //maxLines: 1,overflow: TextOverflow.ellipsis,
+                        Text(
+                          noteList![index].title,
+                          textAlign: TextAlign.center,
+                          style: const TextStyle(
+                              fontWeight: FontWeight.w900,
+                              fontSize: 20,
+                              color: Colors.black),
+                          maxLines: 3,
+                          overflow: TextOverflow.ellipsis,
                         ),
-                      ),
-                      onTap: (){
-                        goToEditPage(noteList![index],'Edit Note');
-                      },
-                      onDoubleTap: (){
-                        _delete(context, noteList![index]);
-                      },
-                    );
-                  }),
-            ),
-          ],
-        ),
+                        const SizedBox(
+                          height: 5,
+                        ),
+                        Text(
+                          noteList![index].date,
+                          style: const TextStyle(
+                              fontWeight: FontWeight.w600,
+                              fontSize: 10,
+                              color: Colors.black54),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        Container(
+                            padding: EdgeInsets.all(5),
+                            child: Text(
+                              noteList![index].description,
+                              textAlign: TextAlign.center,
+                              style: const TextStyle(
+                                  fontWeight: FontWeight.w700,
+                                  fontSize: 14,
+                                  color: Colors.black87),
+                              maxLines: 10,
+                              overflow: TextOverflow.ellipsis,
+                            )),
+                      ],
+                    )),
+                  ),
+                ),
+                onTap: () {
+                  goToEditPage(noteList![index], 'Edit Note');
+                },
+                onDoubleTap: () {
+                  _delete(context, noteList![index]);
+                },
+              );
+            },
+            staggeredTileBuilder: (index) {
+              return StaggeredTile.fit(1);
+            }),
 
+        // body: Column(
+        //   mainAxisAlignment: MainAxisAlignment.center,
+        //   children: [
+        //     const SizedBox(height: 5,),
+        //     const Text("Single Tap to Update"),
+        //     const Text("Double Tap to Delete"),
+        //     const SizedBox(height: 5,),
+        //
+        //     Expanded(
+        //       child: GridView.builder(
+        //           gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+        //
+        //           itemCount: noteList!.length,
+        //
+        //           itemBuilder: (context, index) {
+        //             return GestureDetector(
+        //                 child: Container(
+        //                   //margin: const EdgeInsets.all(10),
+        //                   decoration: BoxDecoration(
+        //                     color: Colors.black87,
+        //                   ),
+        //
+        //                   // width: 160,
+        //                   // height: 250,
+        //                   child: Card(
+        //                     //elevation: 20,
+        //
+        //                     color: Colors.white70, //Color(0xff252525),
+        //
+        //                     shape: RoundedRectangleBorder(
+        //                       borderRadius: BorderRadius.circular(20),
+        //
+        //                     ),
+        //                     child: SingleChildScrollView(
+        //                       child: Container(
+        //
+        //                         //alignment: Alignment.center,
+        //                         margin: EdgeInsets.all(5),
+        //                         child: Column(
+        //
+        //                           mainAxisAlignment: MainAxisAlignment.spaceAround,
+        //                           children: <Widget>[
+        //                             //maxLines: 1,overflow: TextOverflow.ellipsis,
+        //                             Text(noteList![index].title,textAlign: TextAlign.center,style: const TextStyle(fontWeight: FontWeight.w900,fontSize: 20,color: Colors.black),maxLines: 3,overflow: TextOverflow.ellipsis,),
+        //                             const SizedBox(height: 5,),
+        //                             Text(noteList![index].date,style: const TextStyle(fontWeight: FontWeight.w600,fontSize: 10,color: Colors.black54),),
+        //                             const SizedBox(height: 20,),
+        //                             Container(padding: EdgeInsets.all(5),child: Text(noteList![index].description,textAlign: TextAlign.center,style: const TextStyle(fontWeight: FontWeight.w700,fontSize: 14,color: Colors.black87),maxLines: 10,overflow: TextOverflow.ellipsis,)),
+        //                           ],
+        //                         ),
+        //                       ),
+        //                     ),
+        //                   ),
+        //
+        //               ),
+        //               onTap: (){
+        //                 goToEditPage(noteList![index],'Edit Note');
+        //               },
+        //               onDoubleTap: (){
+        //                 _delete(context, noteList![index]);
+        //               },
+        //             );
+        //           }),
+        //     ),
+        //   ],
+        // ),
         floatingActionButton: FloatingActionButton(
           tooltip: "Add",
           child: const Icon(Icons.add),
-          onPressed: (){
-            goToEditPage(Note('','',''),"Add Note");
+          onPressed: () {
+            goToEditPage(Note('', '', ''), "Add Note");
           },
         ),
         floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       ),
     );
   }
-
 
   void goToEditPage(Note note, String appBarTitle) async {
     bool result =
@@ -137,6 +221,4 @@ class _NotesListState extends State<NotesList> {
       });
     });
   }
-
-
 }
